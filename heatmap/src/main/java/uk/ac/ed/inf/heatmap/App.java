939a937baed8bf26d9a036d5ebad5f2d35ff7b2d
wassim2u <wassim.jabrane@gmail.com>
2020-10-23 08:49:23 +0100

    Submitted coursework 1

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -16,11 +16,15 @@ import com.mapbox.geojson.Polygon;
 
 public class App 
 {
-	// Grid dimension size, default is 10 by 10.
+	// Grid dimension size, default is 10 by 10 (as assumed). You can change the GRID_SIZE here.
 	private static final int GRID_SIZE = 10; 
 	
 	//Grid containing predictions made by the researchers to the heat map application
 	private static int[][] predictions = new int[GRID_SIZE][GRID_SIZE]; 
+	
+	//The minimum and maximum limit for the values in the predictions grid.
+	private static final int MIN_LIMIT = 0;
+	private static final int MAX_LIMIT = 255;
 	
 	/*
 	 * The numbers describe the range of values that the latitudes and longitudes could take
@@ -31,8 +35,8 @@ public class App
 	private static final double SOUTHERN_BOUNDARY = 55.942617; //Currently latitude for Top of the Meadows and Buccleuch St. Bus Stop
 	private static final double WESTERN_BOUNDARY = -3.192473;  //Currently Longitude for Forrest Hill and Top of the Meadows
 	private static final double EASTERN_BOUNDARY = -3.184319;  //Currently Longitude for KFC and Buccleuch St. Bus Stop
-	private static final double RANGE_LONGITUDE = Math.abs(EASTERN_BOUNDARY - WESTERN_BOUNDARY);
-	private static final double RANGE_LATITUDE =  Math.abs(NORTHERN_BOUNDARY - SOUTHERN_BOUNDARY);	
+	private static final double RANGE_LONGITUDE = Math.abs(EASTERN_BOUNDARY - WESTERN_BOUNDARY); //width of the area enclosed by the boundaries
+	private static final double RANGE_LATITUDE =  Math.abs(NORTHERN_BOUNDARY - SOUTHERN_BOUNDARY);	//height of the area enclosed by the boundaries
 	
 	//Map colour names to Hex values
 	private static final Map<String,String> colourNameToHex = Map.of(
@@ -58,34 +62,48 @@ public class App
 	    	int row = 0 ; int col = 0;
 	    	for (row = 0; row < predictions.length; row++) {
 	    		String[] line = reader.nextLine().replace(" ","").split(","); //Remove any white space and split the string into a String array of numbers after removing the comma
-	    		for (col = 0; col<predictions.length; col++) {
+	    		
+	    		//Check whether the number of columns is equal to the specified number GRID_SIZE
+	    		int numberOfColumns = line.length;
+	    		if (checkGridSize(numberOfColumns,reader)) {
+	    			reader.close();
+	    			throw new Exception("Grid size is not " + GRID_SIZE + " by " + GRID_SIZE);
+	    		}
+	    		for (col = 0; col<numberOfColumns; col++) {
 	    			predictions[row][col] = Integer.parseInt(line[col]); //Parse each number in the array from String to int type
-	    			if(predictions[row][col]>255 || predictions[row][col]<0) { 
+	    			//Check whether the value is not within MIN_LIMIT and MAX_LIMIT, and throw an error if outside the range. 
+	    			if(checkValue(predictions[row][col])) { 
 	    		    	reader.close();
-	    				throw new Exception("The value \"" + line[col] + "\" in (row " + row + ", column " + col +") is not in the range 0 and 255.");
+	    				throw new Exception("The value \"" + line[col] + "\" in (row " + row + ", column " + col +") is not in the range 0 and 255.\n");
 	    			}
 	    		}
-	    		check10by10GridSize(col,reader); 
 	    	}
-	    	check10by10GridSize(row, reader);
+    		//As an extra test at the end of the for loop after we traverse all the lines in the file, check whether the number of rows is the specified GRID_SIZE.
+	    	if (checkGridSize(row,reader)) {
+    			reader.close();
+    			throw new Exception("Grid size is not " + GRID_SIZE + " by " + GRID_SIZE);
+    		}
+	    	
 	    	reader.close();
 		}
 		catch(Exception e){
    	      	e.printStackTrace();
-   	      	System.out.println("Please provide a 10*10 grid of integer numbers in a text file with the correct range of values 0 <= x <= 255.");
+   	      	System.out.println("Please provide a "+ GRID_SIZE + " by "+ GRID_SIZE + " grid of integer numbers in a text file with the correct range of values 0 <= x <= 255.");
    	      	System.exit(-1); //Unsuccessful termination of program
-    	}
-		
+    	}	
 	}
+	
+	//If one of the values in predictions is greater than MAX_VALUE or less than MIN_VALUE, return True. Otherwise, return False.
+	private static boolean checkValue(int val) {
+		return (val < MIN_LIMIT || val > MAX_LIMIT);
+	}
+	
 	/*
 	 * The implementation assumes that the dimensions of the file text to be inputed in the argument should be 10 by 10.
 	 * This method is used twice when initialising Grid (predictions array), one for checking the row size and another for column size.
 	 */
-	private static void check10by10GridSize(int size, Scanner reader) throws IllegalArgumentException {
-		if (size!=10) {
-			reader.close();
-			throw new IllegalArgumentException("Grid size is not 10 by 10.");
-		}
+	private static boolean checkGridSize(int size, Scanner reader) {
+		return size!=GRID_SIZE;
 	}
 	
 	/*
@@ -134,7 +152,9 @@ public class App
 	return outputCoords;
 	}
 
-	
+	/*
+	 * returns a Json String with Features from a 2D list of points
+	 */
 	private static String createGEOJsonPolygonsfromPoints(List<List<Point>> pointCoords) {
 		List<Feature> listOfFeatures = new ArrayList<>();
 		for(int row=1;row<pointCoords.size();row++) { 
@@ -144,13 +164,8 @@ public class App
 				Point bottomLeftPoint = pointCoords.get(row).get(col-1);
 				Point bottomRightPoint = pointCoords.get(row).get(col);
 				
-				//To create a polygon, the first and last point in the list should be the same enclosing the linear ring.
-				//So, first point is reiterated at the end to close our polygon.
-				List<Point> polygonPointCoords =
-						new ArrayList<>(List.of(upperLeftPoint,upperRightPoint,bottomRightPoint,bottomLeftPoint,upperLeftPoint));
-				
 				//Create polygon from List of points
-				Polygon polygon = Polygon.fromLngLats(List.of(polygonPointCoords));
+				Polygon polygon = createPolygon(upperLeftPoint,upperRightPoint,bottomRightPoint,bottomLeftPoint);
 				
 				//Turn polygon to feature
 				Feature feat = Feature.fromGeometry((Geometry) polygon);
@@ -162,16 +177,28 @@ public class App
 	        	listOfFeatures.add(feat);
 			}
 		}
+		
 		//Turn the list of Features with the added properties to Json String
 		return (FeatureCollection.fromFeatures(listOfFeatures)).toJson(); 
 	}
+	//Helper function to create a Polygon from List of points. The order passed in the parameters is important.
+	private static Polygon createPolygon(Point upperLeft, Point upperRight, Point bottomRight, Point bottomLeft) {
+		//To create a polygon, the first and last point in the list should be the same enclosing the linear ring.
+		//So, first point is reiterated at the end to close our polygon.
+		List<Point> polygonPointCoords =
+				new ArrayList<>(List.of(upperLeft,upperRight,bottomRight,bottomLeft,upperLeft));
+		
+		return Polygon.fromLngLats(List.of(polygonPointCoords));
+	}
+	//Helper function to add suitable attributes
 	private static void addAppropriateProperties(int row, int col, Feature feat) {
 		String colourVal = mapValueToColour(predictions[row][col]);
 		feat.addStringProperty("fill-opacity", FILL_OPACITY);
 		feat.addStringProperty("rgb-string", colourVal);
 		feat.addStringProperty("fill", colourVal);
 	}
-	//Helper class to assign Hex value in order to add a colour attribute
+	
+	//Helper function to assign Hex value in order to add a colour attribute
 	private static String mapValueToColour(int val) {
 		if (val>=0 && val<32) {
 			return colourNameToHex.get("Green"); 
@@ -200,28 +227,34 @@ public class App
 	}
 	
 	private static void createNewFile(String JsonString, String filename) throws IOException {
-		FileWriter myWriter = new FileWriter("heatmap.geojson");
+		FileWriter myWriter = new FileWriter(filename);
     	myWriter.write(JsonString);
     	myWriter.close();
-    	System.out.println("Successfully created heatmap.geojson file.");
+    	System.out.println("Successfully created " + filename + " file.");
 	}
 	
     public static void main( String[] args )
     {
         if (args.length<1) {
         	System.out.println("Please pass a file as an argument in the following format:");
-        	System.out.println("java -jar heatmap.jar filename.txt");
+        	System.out.println("java -jar heatmap.jar /path/to/predictions.txt");
         }
         else {
-            initialiseGrid(args[0]);
+        	//initialises the static predictions array using the text file provided
+            initialiseGrid(args[0]); 
+            
+            //Create points confined within the area of the boundaries
         	List<List<Point>> pointCoords= createPoints();
+        	
+        	//Convert the points to GeoJson String with Polygons having appropriate attributes
         	String geoJson = createGEOJsonPolygonsfromPoints(pointCoords);
         	try {
 				createNewFile(geoJson,"heatmap.geojson");
 			} catch (IOException e) {
 				e.printStackTrace();
+				System.exit(-1); //Unsuccessful termination of program
 			}
-        	System.exit(0); //Successful termination of program
+        	System.exit(0); //Successful termination of program. A heatmap.geojson file should be created at this point.
 
         }
     }

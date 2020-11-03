13ec268d47ea39b3efd93f29b47c6c78f6bdacf3
wassim2u <wassim.jabrane@gmail.com>
2020-11-03 15:48:53 +0000

    Removed private modifiers and made classes public and set variables accessibility scope to default (to include access within package only). Nested classes are also static now.

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/What3WordsDetails.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/What3WordsDetails.java
@@ -2,32 +2,32 @@ package uk.ac.ed.inf.aqmaps;
 
 public class What3WordsDetails {
 	
-	private String country;
-	private W3WSquare Square;
-	private String nearestPlace;
-	private Coordinates buildingCoordinates;
-	private String threeWords; 
-	private String language;
-	private String mapURL;
+	String country;
+	W3WSquare Square;
+	String nearestPlace;
+	Coordinates buildingCoordinates;
+	String threeWords; 
+	String language;
+	String mapURL;
 	
 	
-	private class W3WSquare{
-		private SouthWestPoint SWPoint;
-		private NorthEastPoint NEPoint;
+	public static class W3WSquare{
+		SouthWestPoint SWPoint;
+		NorthEastPoint NEPoint;
 		
-		private class SouthWestPoint{
-			private double longitude;
-			private double latitude;
+		public static class SouthWestPoint{
+			double longitude;
+			double latitude;
 		}
 		private class NorthEastPoint{
-			private double longitude;
-			private double latitude;
+			double longitude;
+			double latitude;
 		}
 	}
 	
-	private class Coordinates{
-		private double longitude;
-		private double latitude;
+	public static class Coordinates{
+		double longitude;
+		double latitude;
 	}
 	
 	

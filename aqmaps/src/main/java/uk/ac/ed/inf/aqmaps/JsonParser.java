f04dc3405b9710a2bb40391ba791cbaf445b0756
wassim2u <wassim.jabrane@gmail.com>
2020-11-03 15:41:08 +0000

    Implemented some functionalities for JsonParser.

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/JsonParser.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/JsonParser.java
@@ -1,37 +1,109 @@
 package uk.ac.ed.inf.aqmaps;
 
-import java.io.IOException;
+import com.google.gson.Gson;
+import java.time.LocalDate;
+
+
+//The following is used to access web server contents
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+//Exceptions Libraries
+import java.io.IOException;
+import java.net.ConnectException;
+
+
 public class JsonParser {
 	
+	//One HttpClient is created, shared between all HttpRequests.
+	private static final HttpClient client = HttpClient.newHttpClient();
+	//Current Server 
+	private static String serverName = "localhost";
+	
+	private static final int NOTFOUNDSTATUS = 404;
+	
 
-	public String connectToPort(int portNumber) throws IOException, InterruptedException {
+	private static String getBodyContent(int portNumber, String path) {
+		var urlString = "http://" + serverName + ":" +portNumber + "/" + path;
+		HttpResponse<String> response = null;
+		try {
+			// Create a new HttpClient with default settings.
+			var request = HttpRequest.newBuilder()
+			.uri(URI.create(urlString))
+			.build();
+			response = client.send(request, BodyHandlers.ofString());
+			
+			//Catch Null errors in the off chance that it happens; Exit the system if null and print source of error
+			if (response == null){
+				throw new NullPointerException("Response recieved is of type null");
+			}
+			
+		}
+		//Executed when the argument passed is not correct
+		catch (IllegalArgumentException e) {
+			System.out.println("URL passed is not syntactically correct!");
+			System.out.println("Please check the urlString argument that is passed when calling URI.create() method.");
+			e.printStackTrace(); 
+			System.exit(1); //Exit the program, indicating that it exited due to an error.
+		}
+		//Executed when web server is not running or not running on the correct port number.
+		catch (ConnectException e) {
+			System.out.println("Fatal error: Unable to connect to " +
+					serverName + " at port " + portNumber + ".");
+			System.exit(1); //Exit the program, indicating that it exited due to an error.
+		}
+		//Catch other Errors that may be thrown by HttpClient object sending request
+		catch(IOException|InterruptedException|NullPointerException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
-		// Create a new HttpClient with default settings.
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-		.uri(URI.create("http://localhost:" + portNumber + "/maps"))
-		.build();
-		// The response object is of class HttpResponse<String>
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		System.out.println(response.getClass());
-		System.out.println(response.statusCode());
+		
+		//TODO: Check what you should do in case Status code is 404. Check if there are any status codes you need 
+		if ( checkResponseNotFound(response.statusCode()) ) {
+				System.out.println("Status: 404 [Not Found]");
+				System.out.println("File Not Found. Check the URL path for any spelling mistakes.");
+				System.out.println("URL passed: " + urlString);
+				System.exit(0);		
+		}
+			
 		System.out.println(response.body());
 		String body = response.body();
-		
-		
 		return response.body();
-		
-		
-
 	}
 	
+	/*
+	 * Helper method that checks the HTTP Status code returned by the HTTP response. 
+	 * @returns True if status code is 404. 
+	 */
 	
+	private static boolean checkResponseNotFound(int status) {
+		return status == NOTFOUNDSTATUS;
+	}
+	
+	public static String parseWhat3WordsDetails(int portNumber,String what3WordsLocation) throws IOException, InterruptedException {
+		var jsonFileName = "details.json";
+		var folderName = "words";
+		var jsonFilePath = folderName + "/" + what3WordsLocation + "/" + jsonFileName;
+		return getBodyContent(portNumber, jsonFilePath);
+	}
+	
+	public static String parseAirQualityData(int portNumber, LocalDate date) throws IOException, InterruptedException {
+		var jsonFileName = "air-quality-data.json";
+		var folderName = "maps";
+		var jsonFilePath = folderName + "/" + date.getYear() + "/" + date.getMonth() + "/" + date.getDayOfMonth() + "/" + jsonFileName;
+		return getBodyContent(portNumber, jsonFilePath);
+	}
+	
+	public static String parseNoFlyZones(int portNumber)  {
+		var jsonFileName = "no-fly-zones.geojson";
+		var folderName = "buildings";
+		var jsonFilePath = folderName + "/" + jsonFileName;
+		return getBodyContent(portNumber, jsonFilePath);
+	}	
 	
 	
 }
